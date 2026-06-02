bf60a0460695a82edaa1d074f2c67473b355c9cf
Harsh-cmd-star <78646510+Harsh-cmd-star@users.noreply.github.com>
2026-06-02 12:38:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="AI Stock Research Assistant",
-    page_icon="📈"
-)
-
-st.title("📈 AI Stock Research Assistant")
-
-ticker = st.text_input(
-    "Enter Stock Ticker",
-    placeholder="AAPL"
-)
-
-if st.button("Analyze"):
-    st.success(f"Analyzing {ticker}")
+st.write("Hello World")
